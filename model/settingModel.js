2379838096b710d98f8bb58a5a580fd19e39a906
Vo Trung Kien <votrungkiennnn98@gmail.com>
2025-05-09 13:49:39 +0700

    Sua loi

--- model/settingModel.js
+++ model/settingModel.js
@@ -4,15 +4,15 @@ const settingSchema = new mongoose.Schema(
   {
     company_name: {
       type: String,
-      required: true,
+      required: [true, "Không được để trống"],
     },
     address: {
       type: String,
-      required: true,
+      required: [true, "Không được để trống"],
     },
-    contact_email: { type: String, required: true },
-    phone_number: { type: String, required: true },
-    logo_url: { type: String, required: true },
+    contact_email: { type: String, required: [true, "Không được để trống"] },
+    phone_number: { type: String, required: [true, "Không được để trống"] },
+    logo_url: { type: String, required: [true, "Không được để trống"] },
   },
   { timestamps: true }
 );
