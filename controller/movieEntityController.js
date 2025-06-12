b9735eade58da12c34b8e57efe4cbddc27326f0c
vuphan1234 <pvvu2002@gmail.com>
2025-06-12 20:09:04 +0700

    feature/OTN-56

--- controller/movieEntityController.js
+++ controller/movieEntityController.js
@@ -39,7 +39,6 @@ exports.checkDuplicateName = catchAsync(async (req, res, next) => {
   next();
 });
 
-exports.getAllMovieEntitiesAdmin = Factory.getAllWithAdmin(movieEntityModel);
 exports.createMovieEntity = Factory.createOne(movieEntityModel);
 exports.getMovieEntityById = Factory.getOne(movieEntityModel);
 exports.getAllMovieEntities = Factory.getAll(movieEntityModel);
