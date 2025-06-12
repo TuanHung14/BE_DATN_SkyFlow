c8c2da6b5bbc784b9a59d62bc207496696dc2a92
vuphan1234 <pvvu2002@gmail.com>
2025-06-12 18:15:35 +0700

    fix showtime

--- routes/seatRouter.js
+++ routes/seatRouter.js
@@ -2,7 +2,5 @@ const express = require('express');
 const seatController = require('../controller/seatController');
 const router = express.Router();
 
-router.get('/', seatController.getAllSeats);
-router.post('/', seatController.createSeat);
 
 module.exports = router;
