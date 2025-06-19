8d39246b669fed2e3a3b28864d850a8091d18c3a
vuphan1234 <pvvu2002@gmail.com>
2025-06-19 23:07:15 +0700

    thêm cronjob tự động xoá showtime

--- cron_jobs/cronjobs.js
+++ cron_jobs/cronjobs.js
@@ -1,10 +1,20 @@
 const cron = require("node-cron");
 const { updateMovieStatusLogic } = require("../services/movieService");
+const Showtime = require("../model/showtimeModel");
 
 module.exports = () => {
   cron.schedule("0 0 * * *", async () => {
     try {
       const count = await updateMovieStatusLogic();
+
+      // Xóa các showtime có showDate trước 2 ngày
+      const daysToKeep = 2;
+      const cutoffDate = new Date(Date.now() - daysToKeep * 24 * 60 * 60 * 1000);
+
+      const result = await Showtime.deleteMany({
+        showDate: { $lt: cutoffDate }
+      });
+
     } catch (error) {
       console.error("Cronjob error:", error);
     }
