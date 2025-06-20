a53519c8286868832b7e14797bcdfb9d8a34537a
vuphan1234 <pvvu2002@gmail.com>
2025-06-20 10:41:19 +0700

    edit showtime xoá mềm trong cronjob

--- cron_jobs/cronjobs.js
+++ cron_jobs/cronjobs.js
@@ -1,22 +1,30 @@
 const cron = require("node-cron");
-const { updateMovieStatusLogic } = require("../services/movieService");
+const {updateMovieStatusLogic} = require("../services/movieService");
 const Showtime = require("../model/showtimeModel");
 
 module.exports = () => {
-  cron.schedule("0 0 * * *", async () => {
-    try {
-      const count = await updateMovieStatusLogic();
+    cron.schedule("0 0 * * *", async () => {
+        try {
+            const count = await updateMovieStatusLogic();
 
-      // Xóa các showtime có showDate trước 2 ngày
-      const daysToKeep = 2;
-      const cutoffDate = new Date(Date.now() - daysToKeep * 24 * 60 * 60 * 1000);
+            // Xóa các showtime có showDate trước 2 ngày
+            const daysToKeep = 2;
+            const cutoffDate = new Date(Date.now() - daysToKeep * 24 * 60 * 60 * 1000);
 
-      const result = await Showtime.deleteMany({
-        showDate: { $lt: cutoffDate }
-      });
+            const result = await Showtime.updateMany(
+                {
+                    showDate: {$lt: cutoffDate},
+                    isDeleted: { $ne: true }
+                },
+                {
+                    $set: {
+                        isDeleted: true
+                    }
+                }
+            );
 
-    } catch (error) {
-      console.error("Cronjob error:", error);
-    }
-  });
+        } catch (error) {
+            console.error("Cronjob error:", error);
+        }
+    });
 };
