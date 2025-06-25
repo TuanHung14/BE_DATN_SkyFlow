cc4996758f34869b963fb2986fe917956cff75ac
Vu Tuan Hung <vhung5912@gmail.com>
2025-06-25 11:18:45 +0700

    feature/socket

--- config/socket.js
+++ config/socket.js
@@ -2,7 +2,12 @@ const socketIO = require('socket.io');
 const { handleSocketEvents } = require('../controller/socketController');
 
 const initializeSocket = (server) => {
-    const io = socketIO(server);
+    const io = socketIO(server, {
+        cors: {
+            origin: "*",
+            methods: ["GET", "POST"]
+        }
+    });
 
     io.on('connection', async (socket) => {
         console.log('A user connected:', socket.id);
