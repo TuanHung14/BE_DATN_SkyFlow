aa012be18f913ba20a0466bd207c2b77407c79e3
Vu Tuan Hung <vhung5912@gmail.com>
2025-08-11 13:24:33 +0700

    feature/fix-voucher

--- model/bookingModel.js
+++ model/bookingModel.js
@@ -2,33 +2,33 @@ const mongoose = require("mongoose");
 
 const bookingSchema = new mongoose.Schema(
     {
-        // seatId: {
-        //     type: mongoose.Schema.Types.ObjectId,
-        //     ref: 'Seat',
-        //     required: [true, 'Seat ID is required']
-        // },
         seatId: {
-            type: String,
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Seat',
             required: [true, 'Seat ID is required']
         },
-        showtimeId: {
-            type: String,
-            required: [true, 'showtimeId ID is required']
-        },
-        userId: {
-            type: String,
-            required: [true, 'userId ID is required']
-        },
+        // seatId: {
+        //     type: String,
+        //     required: [true, 'Seat ID is required']
+        // },
         // showtimeId: {
-        //     type: mongoose.Schema.Types.ObjectId,
-        //     ref: 'Showtime',
-        //     required: [true, 'Showtime ID is required']
+        //     type: String,
+        //     required: [true, 'showtimeId ID is required']
         // },
         // userId: {
-        //     type: mongoose.Schema.Types.ObjectId,
-        //     ref: 'User',
-        //     required: [true, 'User ID is required']
+        //     type: String,
+        //     required: [true, 'userId ID is required']
         // },
+        showtimeId: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Showtime',
+            required: [true, 'Showtime ID is required']
+        },
+        userId: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'User',
+            required: [true, 'User ID is required']
+        },
         status: {
             type: String,
             enum: ['holding', 'pending', 'success'],
