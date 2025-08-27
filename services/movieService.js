35226e4c786e44c4a86345d56426ee261d2baabc
Vu Tuan Hung <vhung5912@gmail.com>
2025-08-27 18:37:08 +0700

    feature/fix-ticket-me

--- services/movieService.js
+++ services/movieService.js
@@ -1,21 +1,21 @@
 // movieService.js
 const Movie = require("../model/movieModel");
-async function updateMovieStatusLogic() {
-  // Có lỗi sẽ throw ra, ko cần try/catch ở đây
-  const today = new Date();
-  today.setHours(0, 0, 0, 0);
 
-  const result = await Movie.updateMany(
+async function updateMovieStatusLogic(today) {
+    const vietnamTime = new Date(today.getTime() + 7 * 60 * 60 * 1000);
+    vietnamTime.setUTCHours(0, 0, 0, 0);
+
+    const result = await Movie.updateMany(
     {
-      releaseDate: { $lte: today },
+      releaseDate: { $lte: vietnamTime },
       status: "COMING_SOON",
     },
     {
       $set: { status: "NOW_SHOWING" },
     }
-  );
+    );
 
-  return result;
+    return result.modifiedCount;
 }
 
 async function getMovies(){
